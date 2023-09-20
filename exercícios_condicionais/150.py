0b54f046559c1b5d7d859c9cabf436d3a5d2ce63
marilia-borgo <marilia.bm01@gmail.com>
2023-09-20 18:55:45 -0300

    typo

--- exercícios_condicionais/150.py
+++ exercícios_condicionais/150.py
@@ -4,7 +4,7 @@ ang = int(input("DIgite angulo em graus"))
 pi = 3.14
 rang = ang*pi/180
 
-if (rang > pi//2 and rang <=pi) or (rang > 3*pi/2 and rang <= 2*pi):
+if (rang > pi/2 and rang <=pi) or (rang > 3*pi/2 and rang <= 2*pi):
     print("seno", math.sin(rang))
 else:
     print("cosseno", math.cos(rang))
